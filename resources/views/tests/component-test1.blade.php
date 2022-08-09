4082c4d3ad8d02f65411231884a43a5ce939155d
日浦敬悟 <hiurakeigo@hiurakeigonoMacBook-Air.local>
2022-08-09 17:19:47 +0900

    プロップ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">ヘッダー1</x-slot>
 test1
     //変数を呼び出すため(:message)を加える
-    <x-tests.card title="タイトル" content="コンテント" :message="$message"/>
+    <x-tests.card title="タイトル1" content="コンテント" :message="$message"/>
+    <x-tests.card title="タイトル2"/>
 </x-tests.app>
