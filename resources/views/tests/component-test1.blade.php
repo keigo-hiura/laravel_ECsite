847c337da5c31f18d69aaad194740d0b5d505422
日浦敬悟 <hiurakeigo@hiurakeigonoMacBook-Air.local>
2022-08-09 17:07:47 +0900

    add variation

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
 test1
-
-    <x-tests.card title="タイトル" content="コンテント"/>
+    //変数を呼び出すため(:message)を加える
+    <x-tests.card title="タイトル" content="コンテント" :message="$message"/>
 </x-tests.app>
